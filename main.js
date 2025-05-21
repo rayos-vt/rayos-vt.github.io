bad7ae8bccab97bfcbd5a8ba941f2acb26677c9a
rayos-vt <143996910+rayos-vt@users.noreply.github.com>
2025-05-20 21:45:49 -0500

    Update and rename main.js to main/scripts/main.js

--- main.js
+++
@@ -1,2 +0,0 @@
-import Chart from 'chart.js/auto';
-import { getRelativePosition } from 'chart.js/helpers';
---
+++ main/scripts/main.js
@@ -0,0 +1,2 @@
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
