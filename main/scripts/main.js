9f9dc765d5c0434cf77999fd2c2a2c337d8af0c9
rayos-vt <143996910+rayos-vt@users.noreply.github.com>
2025-06-15 22:54:58 -0500

    Update main.js

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -1,2 +1,29 @@
 import Chart from 'chart.js/auto';
 import { getRelativePosition } from 'chart.js/helpers';
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: '192.168.1.31', 
+    user:'root', 
+    password: 'WriteR_AdmiN1',
+    database: '100gf_stocks',
+    connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) conn.end();
+  }
+}
+asyncFunction().then(() => {
+  pool.end();
+});
